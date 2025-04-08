837059ccdca88c02e6d7a73b7d4d3f250770a831
PFBregon <bregon@gmail.com>
2025-04-08 15:21:43 +0200

    footer app created

---
+++ src/components/Footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export const Footer = () => {
+  return (
+    <footer className="bg-[#00328C] text-white text-center p-4 mt-10 font-bree">
+      <div className="container mx-auto flex flex-col md:flex-row justify-between items-start md:items-center text-sm">
+        <div className="md:text-left text-center w-full md:w-1/2 mb-2 md:mb-0">
+          <p>2025 © Arkímedes. Todos los derechos reservados</p>
+        </div>
+        <div className="md:text-right text-center w-full md:w-1/2 flex justify-end gap-12">
+          <p>Términos de uso</p>
+          <p>Política de Privacidad</p>
+        </div>
+      </div>
+    </footer>
+  );
+};
