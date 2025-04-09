e5b14cae853f3f4f3ca8c130da141a786b8b7e74
JesusEnjamio <jenjamiob@gmail.com>
2025-04-09 08:54:06 +0200

    navbar done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
+import { Navbar } from './components/navbar';
 import './styles/styles.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 function App() {
   return (
-    <>
-     <h1 className='text-3xl text-blue-500'>Hola React + Tailwind</h1>
-    </>
-  )
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/mis-paquetes" element={<div>Mis paquetes</div>} />
+        <Route path="/mis-notificaciones" element={<div>Mis notificaciones</div>} />
+        <Route path="/mi-cuenta" element={<div>Mi cuenta</div>} />
+        <Route path="/cerrar-sesion" element={<div>Cerrar sesión</div>} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
