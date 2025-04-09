873f205c884dc2ecdc94cb7c74fe22697b9c5da3
JesusEnjamio <jenjamiob@gmail.com>
2025-04-09 12:33:04 +0200

    hide navigation links on homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,20 @@
-import { Navbar } from './components/navbar';
-import './styles/styles.css';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { AppRouter } from "./routing/AppRouter";
+
+
+import "./styles/styles.css";
+
 function App() {
-  return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/mis-paquetes" element={<div>Mis paquetes</div>} />
-        <Route path="/mis-notificaciones" element={<div>Mis notificaciones</div>} />
-        <Route path="/mi-cuenta" element={<div>Mi cuenta</div>} />
-        <Route path="/cerrar-sesion" element={<div>Cerrar sesión</div>} />
-      </Routes>
-    </Router>
-  );
+
+
+  return <AppRouter />;
+
+
+
+
+
 }
+
+
+
 
 export default App;
