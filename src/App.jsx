d8f1f8d2454527fe24038734ad36b9281cafbb02
Rosa <angelica-2025@outlook.es>
2025-04-09 17:02:39 +0200

    fix form conflict

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
+import { FormularioRegistro } from './components/FormularioRegistro';
 import './styles/styles.css';
 import React from 'react';
 function App() {
   return (
     <>
-     <h1 className='text-3xl text-blue-500'>Hola React + Tailwind</h1>
+     <FormularioRegistro />
     </>
   )
 }
