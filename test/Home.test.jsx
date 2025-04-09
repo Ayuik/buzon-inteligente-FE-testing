b8716760a78c9df17a7ff255a0c7885586728d60
erikamc99 <erikamc99@gmail.com>
2025-04-09 13:06:34 +0200

    Home test, it should render both buttons with correct text, passed

---
+++ test/Home.test.jsx
@@ -0,0 +1,15 @@
+import { render, screen } from "@testing-library/react";
+import { describe, it, expect } from "vitest";
+import { Home } from "../src/components/home/Home"; 
+
+describe("Home component", () => {
+  it("should render both buttons with correct text", () => {
+    render(<Home />);
+
+    const loginButton = screen.getByRole("button", { name: /Iniciar sesión/i });
+    const registerButton = screen.getByRole("button", { name: /Registrarse/i });
+
+    expect(loginButton).toBeInTheDocument();
+    expect(registerButton).toBeInTheDocument();
+  });
+});
