e1b36969a9e69c5c5b5b5749bbf55f0b6bb605f3
Karina Lorenzo <karinalrg16@gmail.com>
2025-04-11 16:35:27 +0200

    fix: changes to the  Button and the ProfileCard

--- src/components/profile/Button.jsx
+++ src/components/profile/Button.jsx
@@ -1,6 +1,9 @@
 export const Button = ({ text, onClick, type = "button" }) => (
-    <button type={type} onClick={onClick} className="bg-[#00328c] text-[#ffffff] w-[184px] h-[54px] m-[center] rounded-[32px] text-[22px] border-[none] cursor-pointer">
-        {text}
-    </button>
+  <button
+    type={type}
+    onClick={onClick}
+    className="bg-[#00328c] text-white w-[170px] h-[45px] rounded-[32px] text-[18px] border-[none] mx-auto cursor-pointer"
+  >
+    {text}
+  </button>
 );
-
--- src/components/profile/ProfileCard.jsx
+++ src/components/profile/ProfileCard.jsx
@@ -3,48 +3,82 @@ import { Button } from "./Button";
 import { useState } from "react";
 
 export const ProfileCard = () => {
-    const [editable, setEditable] = useState(false);
-    // Datos de ejemplo - llamada a la API
-    const [formData, setFormData] = useState({
-        name: 'John',
-        surname: 'Doe',
-        dni: '12345678F',
-        email: 'johndoe@gmail.com',
-        password: '12345678',
-        credential: '1234567ASD'
-    });
+  const [editable, setEditable] = useState(false);
+  // Datos de ejemplo - llamada a la API
+  const [formData, setFormData] = useState({
+    name: "Paco",
+    surname: "Porras Pérez",
+    dni: "12345678F",
+    email: "paquitopp@gmail.com",
+    password: "**********",
+    credential: "1234567ASD",
+  });
 
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        setFormData(prev => ({ ...prev, [name]: value }));
-    };
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({ ...prev, [name]: value }));
+  };
 
-    const handleEdit = () => setEditable(true);
-    const handleCancel = () => setEditable(false);
-    const handleSave = () => {
-        // aqui iría el PUT a la API
-        setEditable(false);
-    };
+  const handleEdit = () => setEditable(true);
+  const handleCancel = () => setEditable(false);
+  const handleSave = () => {
+    // aqui iría el PUT a la API
+    setEditable(false);
+  };
 
-    return (
-        <div className="bg-white p-6 rounded shadow-md max-w-md mx-auto mt-10">
-            <ProfileField label="Nombre:" value={formData.name} editable={editable} onChange={handleChange} name="name" />
-            <ProfileField label="Apellidos:" value={formData.surname} editable={editable} onChange={handleChange} name="surname" />
-            <ProfileField label="DNI:" value={formData.dni} editable={editable} onChange={handleChange} name="dni" />
-            <ProfileField label="Correo Electrónico:" value={formData.email} editable={editable} onChange={handleChange} name="email" />
-            <ProfileField label="Contraseña:" value={formData.password} editable={editable} onChange={handleChange} name="password" />
-            <ProfileField label="Credencial Permanente:" value={formData.credential} editable={false} />
+  return (
+    <div className="bg-[rgba(190,199,255,0.28)] p-6 rounded-[48px] shadow-md max-w-md mx-auto mt-10 font-bree sm:p-12 space-y-10 ">
+      <ProfileField
+        label="Nombre:"
+        value={formData.name}
+        editable={editable}
+        onChange={handleChange}
+        name="name"
+      />
+      <ProfileField
+        label="Apellidos:"
+        value={formData.surname}
+        editable={editable}
+        onChange={handleChange}
+        name="surname"
+      />
+      <ProfileField
+        label="DNI:"
+        value={formData.dni}
+        editable={editable}
+        onChange={handleChange}
+        name="dni"
+      />
+      <ProfileField
+        label="Correo Electrónico:"
+        value={formData.email}
+        editable={editable}
+        onChange={handleChange}
+        name="email"
+      />
+      <ProfileField
+        label="Contraseña:"
+        value={formData.password}
+        editable={editable}
+        onChange={handleChange}
+        name="password"
+      />
+      <ProfileField
+        label="Credencial Permanente:"
+        value={formData.credential}
+        editable={false}
+      />
 
-            <div className="flex justify-end space-x-2 mt-4">
-                {editable ? (
-                    <>
-                        <Button text="Guardar cambios" onClick={handleSave} />
-                        <Button text="Cancelar" onClick={handleCancel}/>
-                    </>
-                ) : (
-                    <Button text="Editar" onClick={handleEdit} />
-                )}
-            </div>
-        </div>
-    );
+      <div className="flex justify-end space-x-2 mt-4">
+        {editable ? (
+          <>
+            <Button text="Guardar cambios" onClick={handleSave} />
+            <Button text="Cancelar" onClick={handleCancel} />
+          </>
+        ) : (
+          <Button text="Editar datos" onClick={handleEdit} />
+        )}
+      </div>
+    </div>
+  );
 };
