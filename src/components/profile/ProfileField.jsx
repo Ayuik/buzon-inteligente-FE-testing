48d753c12a179af1335bd526099e44d8d4f51a28
Celia Garrido <celiagarridoherrera@gmail.com>
2025-04-11 09:33:03 +0200

    Create component ProfileField for changes on data profile

---
+++ src/components/profile/ProfileField.jsx
@@ -0,0 +1,14 @@
+export const ProfileField = ({ label, value, editable, onChange, name }) => (
+    <div className="mb-2">
+        <label className="font-semibold">{label}</label>
+        {editable ? (
+            <input className="w-full border border-gray-300 px-2 py-1 rounded"
+                value = {value}
+                onChange = {onChange}
+                name = {name}
+            />
+        ) : (
+            <p>{value}</p>
+        )}
+    </div>
+);
