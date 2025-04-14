2999ecba6eb97ddaa938aa435bd139e3e55ca65e
JesusEnjamio <jenjamiob@gmail.com>
2025-04-14 11:14:23 +0200

    connect loginForm to backend authentication endpoint

--- src/components/LoginSuccessPopover.jsx
+++ src/components/LoginSuccessPopover.jsx
@@ -1,26 +1,38 @@
-import { Link } from "react-router-dom";
 import { forwardRef } from "react";
+import { Link } from "react-router-dom";
 
-export const LoginSuccessPopover = forwardRef(({ title, message, to, buttonLabel }, ref) => {
-  return (
-    <div
-      ref={ref}
-      id="success-popover"
-      className="absolute left-1/2 top-16 -translate-x-1/2 z-50 inline-block w-80 text-sm text-[#00174B] transition-opacity duration-300 bg-[#E1E5FF] border border-[#00174B] rounded-lg shadow-lg opacity-0 invisible"
-    >
-      <div className="px-4 py-3 border-b border-[#00174B] rounded-t-lg">
-        <h3 className="font-semibold text-center text-lg">{title}</h3>
-      </div>
-      <div className="px-4 py-2 text-center">
-        <p>{message}</p>
-        <Link
-          to={to}
-          id="popover-accept"
-          className="inline-block bg-[#00174B] hover:opacity-90 text-white px-4 py-2 rounded-md mt-3"
+export const LoginSuccessPopover = forwardRef(
+  ({ title = "Login exitoso", message, buttonLabel = "Aceptar", to = "/packages", onAccept }, ref) => {
+    return (
+      <div
+        id="success-popover"
+        ref={ref}
+        className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-50 w-[35%] bg-[#E1E5FF] text-[#00174B] shadow-lg border border-gray-300 rounded-2xl opacity-0 invisible transition-opacity duration-300 font-bree"
+      >
+        <div
+          id="success-popover-header"
+          className="px-6 py-4 border-[#00174B] rounded-t-2xl text-center"
         >
-          {buttonLabel}
-        </Link>
+          <h3 id="success-message-title" className="text-lg font-semibold">
+            {title}
+          </h3>
+          <p className="mt-2">{message}</p>
+        </div>
+        <div
+          id="success-popover-body"
+          className="px-6 py-4 flex justify-center"
+        >
+          <Link to={to}>
+            <button
+              id="success-accept-button"
+              onClick={onAccept}
+              className="bg-[#00328C] hover:opacity-90 text-white px-6 py-2 rounded-full transition"
+            >
+              {buttonLabel}
+            </button>
+          </Link>
+        </div>
       </div>
-    </div>
-  );
-});
+    );
+  }
+);
