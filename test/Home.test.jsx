4a254364800b0ebe0a23b11e353e8037ef1e84d7
Erika <erikamc99.com>
2025-04-15 13:01:11 +0200

    refactor Home second test to include the expects, test passed

--- test/Home.test.jsx
+++ test/Home.test.jsx
@@ -1,5 +1,5 @@
 import { render, screen } from "@testing-library/react";
-import { describe, it, expect } from "vitest";
+import { describe, it, expect, vi } from "vitest";
 import { Home } from "../src/components/home/Home"; 
 
 describe("Home component", () => {
@@ -13,10 +13,19 @@ describe("Home component", () => {
     expect(registerButton).toBeInTheDocument();
   });
 
-    it("funcionalidad de los botones", () => {
-      const onLoginClick = vi.fn();
-      const onRegisterClick = vi.fn();
+  it("funcionalidad de los botones", () => {
+    const onLoginClick = vi.fn();
+    const onRegisterClick = vi.fn();
 
-      render(<Home onLoginClick={onLoginClick} onRegisterClick={onRegisterClick} />);
+    render(<Home onLoginClick={onLoginClick} onRegisterClick={onRegisterClick} />);
+
+    const loginButton = screen.getByRole("button", { name: /Iniciar sesión/i });
+    const registerButton = screen.getByRole("button", { name: /Registrarse/i });
+
+    loginButton.click();
+    registerButton.click();
+
+    expect(onLoginClick).toHaveBeenCalledTimes(1);
+    expect(onRegisterClick).toHaveBeenCalledTimes(1);
   });
 });
