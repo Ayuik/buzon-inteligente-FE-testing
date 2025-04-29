b68d357f5626c889e7e52c99cb622f135f0b4b3c
AlejandroAriasSL <alejandroasilva02@gmail>
2025-04-29 20:20:58 +0200

    fix redirect to dahsboard if token available and is logged in

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,7 +1,18 @@
 import { Link } from "react-router-dom";
 import notFound  from "../assets/not_found.png";
+import { useAuth } from "../context/AuthProvider";
+import { useEffect, useState } from "react";
 
 export const NotFoundPage = () => {
+    const { authState } = useAuth();
+    const [destination, setDestination] = useState("/");
+
+    useEffect(() => {
+      if(authState && localStorage.getItem("token")) 
+        { setDestination("/user/packages") }
+
+    }, [authState])
+
   return (
     <section className="flex flex-col justify-center gap-y-5 p-5 text-center min-h-screen font-bree">
       <div className="inline-block mx-auto">
@@ -11,7 +22,7 @@ export const NotFoundPage = () => {
         <h1 className="inline-block mx-auto text-4xl">404</h1>
         <p className="inline-block mx-auto text-4xl">Página no encontrada</p>
         <Link
-          to="/"
+          to={destination}
           className="inline-block mx-auto p-4 hover:bg-[#00328C] text-xl text-white bg-[#4574FF] rounded-xl"
         >
           Vuelve a la página principal
