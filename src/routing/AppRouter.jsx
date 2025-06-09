3c93f6a8e250c25cfba27a621bb8568e66465f1a
MartaBernardoZamora <martabernardozamora@gmail.com>
2025-06-09 10:53:41 +0200

    feat: ProfileProvider context call into AppRouter

--- src/routing/AppRouter.jsx
+++ src/routing/AppRouter.jsx
@@ -1,13 +1,14 @@
 import { RouterProvider } from "react-router-dom";
 import { router } from "./Router";
 import { AuthProvider } from "../context/AuthProvider";
+import { ProfileProvider } from "../context/ProfileProvider";
 
 export const AppRouter = () => {
   return(
     <AuthProvider>
-
-      <RouterProvider router={router} />
-
+      <ProfileProvider>
+        <RouterProvider router={router} />
+      </ProfileProvider>
     </AuthProvider>
   ) 
 };
