3d022df2ec41b3f2d9509047bc007bd72eb799f3
MartaBernardoZamora <martabernardozamora@gmail.com>
2025-06-09 15:10:06 +0200

    fix: sync issue in ProfileProvider useEffect

--- src/context/ProfileProvider.jsx
+++ src/context/ProfileProvider.jsx
@@ -5,23 +5,25 @@ import { useAuth } from "./AuthProvider";
 const ProfileContext = createContext();
 
 export const ProfileProvider = ({ children }) => {
-    const [userProfile, setUserProfile] = useState(null);
+    const [userProfile, setUserProfile] = useState('');
     const { userId } = useAuth();
+    
     useEffect(() => {
         const token = localStorage.getItem('token');
         if (!token) {
-            return;//accion si no estas logeado
+            return;
         }
+
         const validateProfile = async () => {
             try{
-                const { profile } = await getUserProfile(userId, token);
+                const profile = await getUserProfile(userId, token);
                 setUserProfile(profile);
             }catch(error){
                 console.log(error);
             }
         }
         validateProfile();
-    }, []);
+    }, [userId]);
     return ( 
         <ProfileContext.Provider value={{userProfile}}>
             {children}
